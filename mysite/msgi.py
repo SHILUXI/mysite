9b66ef13c04392aed1f401c87ed08c8ac5cf05aa
SHILUXI <2417565941@qq.com>
2019-12-25 19:05:22 +0800

    static

--- mysite/msgi.py
+++ mysite/msgi.py
@@ -1,7 +1,8 @@
 import os
-
-from django.core.wsgi import get_wsgi_application
-
+import sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
+sys.path.append('/home/mysite/mysite')
+ 
+from django.core.wsgi import get_wsgi_application
+ 
 application = get_wsgi_application()
